4bee7fd02e9d95256c30577d7393282ebfc2c69f
vinika02 <you@example.com>
2021-12-11 21:30:58 +0800

    name-mail updates

--- src/pages/name-mail.js
+++ src/pages/name-mail.js
@@ -7,18 +7,17 @@ const fontName = {
 export default function Namemail() {
     return(
         <div className="col">
-            <div className="pt-5">
-                What is your name and email?
-                <div className="col-12">
-                    <form>
-                        <input type="text" class="form-control" placeholder="First Name" required=""></input> 
-                        <input type="last name" class="form-control mt-2" placeholder="Last Name" required=""></input>
-                        <input type="email" class="form-control mt-2" placeholder="Email" required=""></input>
-                        <button type="continue" class="btn btn-dark mt-2">Continue</button>
-                    </form>
+            What is your name and email?
+                <div className="col">
+                    <div className="col-sm-12">
+                        <form>
+                            <input type="text" class="form-control" placeholder="First Name" required=""></input> 
+                            <input type="last name" class="form-control mt-2" placeholder="Last Name" required=""></input>
+                            <input type="email" class="form-control mt-2" placeholder="Email" required=""></input>
+                            <button type="continue" class="btn btn-dark mt-2">Continue</button>
+                        </form>
+                    </div>    
                 </div>
-            </div>
         </div>
-
     );
 }
