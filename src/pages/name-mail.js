269046d02094029891dd7a82549fe0159b06e0b0
Harrold1995 <harroldvanmartinez@gmail.com>
2021-12-11 22:56:34 +0800

    confirm email page updates

--- src/pages/name-mail.js
+++ src/pages/name-mail.js
@@ -1,30 +1,42 @@
 import React from "react";
-import Layout from "../components/Layout";
+import { Helmet } from "react-helmet";
 
-const fontName = {
-    fontFamily: 'arial',
-};
+
+const verticalCenter = {
+    'margin-top':'300px'
+}
 export default function Namemail() {
     return(
-        <div className="col-sm-5">
-            <p className="bg-primary">What is your name and email?</p>
-            <div className="mt-5">
-                Please enter your name and email address below
-                    <div className="row">
-                        <div className="col-sm-12"> 
-                            <input type="text" class="form-control" placeholder="First Name" required=""></input> 
+        <>
+            <Helmet>                            
+                <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+                <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"/>
+                <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+            </Helmet>
+            <div className="container">
+                <div className="col-sm-4 offset-sm-4" style={verticalCenter}>
+                    <p className="fs-3 fw-bold">What is your name and email?</p>
+                    <div className="mt-3">
+                        <div className="mb-4">Please enter your name and email address below</div>  
+                        <div className="row">
+                            <div className="col-sm-12"> 
+                                <input type="text" className="form-control" placeholder="First Name" required=""></input> 
+                            </div>
+                            <div className="col-sm-12">
+                                <input type="last name" className="form-control mt-2" placeholder="Last Name" required=""></input>     
+                            </div>
+                            <div className="col-sm-12">
+                                <input type="email" className="form-control mt-2" placeholder="Email" required=""></input>
+                            </div>
+                            <div className="col-sm-12 mt-2">
+                                <button type="continue" className="btn btn-primary mt-2">Continue</button>  
+                            </div>
                         </div>
-                        <div className="col-sm-12">
-                            <input type="last name" class="form-control mt-2" placeholder="Last Name" required=""></input>     
-                        </div>
-                        <div className="col-sm-12">
-                            <input type="email" class="form-control mt-2" placeholder="Email" required=""></input>
-                        </div>
-                        <div className="col-sm-12">
-                            <button type="continue" class="btn btn-dark mt-2">Continue</button>  
-                        </div>
-                    </div>
-            </div>        
-        </div>
+                    </div>        
+                </div>
+            </div>
+
+        </>        
+        
     );
 }
